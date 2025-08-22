8f6a0c958e66f376b00c3b932f13ed177bf6eef1
Michal Ziulek <michalziulek@gmail.com>
2025-08-22 20:27:44 +0200

    update

--- src/ex1.c
+++ src/ex1.c
@@ -2,7 +2,7 @@
 
 void ex1_run(void)
 {
-  exn_create_window(2048, 2048, "magiciseverywhere: ex1");
+  exn_create_window(1024, 1024, "magiciseverywhere: ex1");
   do {
     glClearBufferfv(GL_COLOR, 0, (float[]){ 0.2f, 0.4f, 0.8f, 1.0 });
   } while (exn_update_window("magiciseverywhere: ex1"));
